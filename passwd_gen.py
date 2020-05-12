7a1ecf8d6e5242a7618d81005e9ad135671b712c
Joe <11597133+m33k@users.noreply.github.com>
2020-05-12 03:02:59 +0000

    REMOVED: print statement for numbers

--- passwd_gen.py
+++ passwd_gen.py
@@ -8,8 +8,6 @@ string = string.ascii_letters + string.digits
 parser = argparse.ArgumentParser(description='A basic password generator')
 parser.add_argument('integers', nargs='?', type=int, default=10, help='Add an integer such as 17 to set the length of the password. If none is given, the default will be used')
 number = parser.parse_args()
-print(number)
-print(vars(number))
 
 # https://stackoverflow.com/questions/16878315/what-is-the-right-way-to-treat-python-argparse-namespace-as-a-dictionary
 
