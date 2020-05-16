e533b5531b9a58a91c088caf9bd3c97a49e4f1af
Joe <11597133+m33k@users.noreply.github.com>
2020-05-16 02:36:38 +0000

    DELETED: uncessary exit code

--- passwd_gen.py
+++ passwd_gen.py
@@ -18,7 +18,6 @@ if arg < 63:
     pass
 else:
     raise ValueError("Password length greater than 62 is not support at this time")
-    #exit(code=1)
 
 passwd = random.sample(string, inputs['integer'])
 print("".join(passwd)) # Prints Password without array
