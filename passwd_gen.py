a16560947f13d9b02af8fbde9d20cb533b644a99
Joe <11597133+m33k@users.noreply.github.com>
2020-05-27 03:54:02 +0000

    issue #8 - WIP

--- passwd_gen.py
+++ passwd_gen.py
@@ -4,6 +4,8 @@ import random
 import argparse
 
 string = string.ascii_letters + string.digits
+#simple_string = string.ascii_letters + string.digits
+complex_string = string.ascii_lowercase + string.ascii_uppercase + string.punctuation + string.digits
 parser = argparse.ArgumentParser(description='A basic password generator')
 parser.add_argument('integer', nargs='?', type=int, default=10, help='Add an integer such as 17 to set the length of the password. If none is given, the default will be used')
 arg = parser.parse_args()
@@ -16,10 +18,10 @@ arg = inputs['integer']
 
 if arg < 63:
     pass
+    passwd = random.sample(simple_string, inputs['integer'])
+    #passwd = random.sample(complex_string, inputs['integer'])
+    print("".join(passwd)) # Prints Password without array
 else:
     raise ValueError("Password length greater than 62 is not support at this time")
-
-passwd = random.sample(string, inputs['integer'])
-print("".join(passwd)) # Prints Password without array
 
 
