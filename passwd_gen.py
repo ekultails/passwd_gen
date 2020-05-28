698575327b6dbab3ea6a7bab4fe99509533685ac
Joe <11597133+m33k@users.noreply.github.com>
2020-05-28 00:18:28 +0000

    ADDED: logic for passwd complexity boolean

--- passwd_gen.py
+++ passwd_gen.py
@@ -11,15 +11,20 @@ arg = parser.parse_args()
 
 # https://stackoverflow.com/questions/16878315/what-is-the-right-way-to-treat-python-argparse-namespace-as-a-dictionary
 
+#passwd = []
+passwd_complexity = False
 inputs = vars(arg)
 
 arg = inputs['integer']
-
 if arg < 63:
     pass
-    passwd = random.sample(simple_string, inputs['integer'])
-    #passwd = random.sample(complex_string, inputs['integer'])
-    print("".join(passwd)) # Prints Password without array
 else:
     raise ValueError("Password length greater than 62 is not support at this time")
+
+if passwd_complexity == True:
+    passwd = random.sample(complex_string, inputs['integer'])
+elif passwd_complexity == False:
+    passwd = random.sample(simple_string, inputs['integer'])
+
+print("".join(passwd)) # Prints Password without array
 
