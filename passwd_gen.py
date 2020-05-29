71d98e645688912a29f9b7e26e9964eab9c75d5b
Joe <11597133+m33k@users.noreply.github.com>
2020-05-29 03:15:57 +0000

    ADDED: logic to enable complex passwords

--- passwd_gen.py
+++ passwd_gen.py
@@ -7,12 +7,20 @@ simple_string = string.ascii_letters + string.digits
 complex_string = string.ascii_lowercase + string.ascii_uppercase + string.punctuation + string.digits
 parser = argparse.ArgumentParser(description='A basic password generator')
 parser.add_argument('integer', nargs='?', type=int, default=10, help='Add an integer such as 17 to set the length of the password. If none is given, the default will be used')
+parser.add_argument('-c', help="enable strong passwords", action="store_true")
 arg = parser.parse_args()
 
 # https://stackoverflow.com/questions/16878315/what-is-the-right-way-to-treat-python-argparse-namespace-as-a-dictionary
 
 #passwd = []
 passwd_complexity = False
+inputs = vars(arg)
+
+if arg.c:
+    passwd_complexity = True
+else:
+    pass
+
 inputs = vars(arg)
 
 int = inputs['integer']
